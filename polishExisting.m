4dea9c19e908a35b965e868526d6cea42a9237ca
Dinesh Ramasamy <iitmdinesh@gmail.com>
2014-10-19 09:24:24 -0700

    Computationaly expensive version of refinement

--- polishExisting.m
+++ polishExisting.m
@@ -21,20 +21,20 @@ K = length(omegaFine);
 sinusoid    = @(omega) exp(1j*(0:(N-1))'*omega)/sqrt(N);
 d_sinusoid  = @(omega) 1j*(0:(N-1))'...
     .*exp(1j*(0:(N-1))'*omega)/sqrt(N);
-d2_sinusoid  = @(omega) -(((0:(N-1))').^2)...
-    .*exp(1j*(0:(N-1))'*omega)/sqrt(N);
+% d2_sinusoid  = @(omega) -(((0:(N-1))').^2)...
+    % .*exp(1j*(0:(N-1))'*omega)/sqrt(N);
 
 
 omegaFine = omegaFine(:);
 
 S_IFFT      = zeros(M,K);
 DER_S_IFFT  = zeros(M,K);
-DER2_S_IFFT = zeros(M,K);
+% DER2_S_IFFT = zeros(M,K);
 
 for count = 1:K
     S_IFFT(:,count)      = S * sinusoid(omegaFine(count));
     DER_S_IFFT(:,count)  = S * d_sinusoid(omegaFine(count));
-    DER2_S_IFFT(:,count) = S * d2_sinusoid(omegaFine(count));
+    % DER2_S_IFFT(:,count) = S * d2_sinusoid(omegaFine(count));
 end
 
 for step_count = 1:numsteps
@@ -43,14 +43,14 @@ for step_count = 1:numsteps
     y_r = y - S_IFFT*gainEst;
     
     DER_S_IFFT_times_gain  = DER_S_IFFT*diag(gainEst);
-    DER2_S_IFFT_times_gain = DER2_S_IFFT*diag(gainEst);
+    % DER2_S_IFFT_times_gain = DER2_S_IFFT*diag(gainEst);
     
-    % omega_jump = real(DER_S_IFFT_times_gain'*DER_S_IFFT_times_gain)\...
-    %     real(DER_S_IFFT_times_gain'*y_r);
+    omega_jump = real(DER_S_IFFT_times_gain'*DER_S_IFFT_times_gain)\...
+        real(DER_S_IFFT_times_gain'*y_r);
     
     % Newton step
-    omega_jump = real(DER_S_IFFT_times_gain'*DER_S_IFFT_times_gain -...
-        diag(DER2_S_IFFT_times_gain'*y_r))\real(DER_S_IFFT_times_gain'*y_r);
+    % omega_jump = real(DER_S_IFFT_times_gain'*DER_S_IFFT_times_gain -...
+        % diag(DER2_S_IFFT_times_gain'*y_r))\real(DER_S_IFFT_times_gain'*y_r);
     
     omegaDelta = max(min(omega_jump, maxjump),-maxjump);
     omegaFine = omegaFine + omegaDelta;
@@ -59,7 +59,7 @@ for step_count = 1:numsteps
         S_IFFT(:,count)     = S * sinusoid(omegaFine(count));
         if step_count ~= numsteps
             DER_S_IFFT(:,count)  = S * d_sinusoid(omegaFine(count));
-            DER2_S_IFFT(:,count) = S * d2_sinusoid(omegaFine(count));
+            % DER2_S_IFFT(:,count) = S * d2_sinusoid(omegaFine(count));
         end
     end
     
