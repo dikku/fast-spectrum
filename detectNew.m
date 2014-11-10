6dd6b0599dacd051054f28339de3e5559b4e4aff
Dinesh Ramasamy <dineshr@ece.ucsb.edu>
2014-11-10 14:27:50 -0800

    returns freq, gain and residue

--- detectNew.m
+++ detectNew.m
@@ -1,10 +1,14 @@
-function omega_add = detectNew(y_r, sampledManifold)
+function [omega_add, gain_add, y_r] = detectNew(y, sampledManifold)
 
 % adds a new frequency by correlating the residue with the coarse 
 % templates in sampledManifold.map_IfftMat
 
-xcorr = abs(y_r'*sampledManifold.map_IfftMat)...
+xcorr = abs(y'*sampledManifold.map_IfftMat)...
     ./sampledManifold.map_IfftMat_norm_sq;
 [~, which_bin]  = max(xcorr);
 
 omega_add = sampledManifold.coarseOmega(which_bin);
+gain_add  = (sampledManifold.map_IfftMat(:,which_bin)' * y)/...
+    sampledManifold.map_IfftMat_norm_sq(which_bin);
+
+y_r = y - gain_add*sampledManifold.map_IfftMat(:,which_bin);
