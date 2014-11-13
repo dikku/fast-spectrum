ff0a94739ece56d5c852b6751aebcbadfb199829
Dinesh Ramasamy <dineshr@ece.ucsb.edu>
2014-11-13 14:59:10 -0800

    changed the case when measurements are not compressive (faster using FFTs)

--- detectNew.m
+++ detectNew.m
@@ -1,14 +1,34 @@
-function [omega_add, gain_add, y_r] = detectNew(y, sampledManifold)
+function [omega, gain, y_r] = detectNew(y_r, sampledManifold)
 
 % adds a new frequency by correlating the residue with the coarse 
 % templates in sampledManifold.map_IfftMat
 
-xcorr = abs(y'*sampledManifold.map_IfftMat)...
-    ./sampledManifold.map_IfftMat_norm_sq;
-[~, which_bin]  = max(xcorr);
+if sampledManifold.is_eye
+    R = length(sampledManifold.coarseOmega);
+    N = length(y_r);
+    
+    possible_gains  = fft(y_r, R)/sqrt(N); 
+    % phases are wrong at this point 
+    % we only correct the phases 
+    % for the detected frequency
+else
+    possible_gains = (sampledManifold.map_IfftMat'*y_r)...
+        ./sampledManifold.map_IfftMat_norm_sq.';
+end
 
-omega_add = sampledManifold.coarseOmega(which_bin);
-gain_add  = (sampledManifold.map_IfftMat(:,which_bin)' * y)/...
-    sampledManifold.map_IfftMat_norm_sq(which_bin);
+[~, which_bin]  = max(abs(possible_gains));
 
-y_r = y - gain_add*sampledManifold.map_IfftMat(:,which_bin);
+omega = sampledManifold.coarseOmega(which_bin);
+gain  = possible_gains(which_bin);
+
+% correct phases to reflect definition of manifold
+if and(sampledManifold.is_eye, sampledManifold.ant_idx(1)~=0)
+    gain = gain.*exp(-1j*omega*sampledManifold.ant_idx(1));
+end
+
+% compute new residue
+if sampledManifold.is_eye
+    y_r = y_r - gain*exp(1j*omega*sampledManifold.ant_idx(:))/sqrt(N);
+else
+    y_r = y_r - gain*sampledManifold.map_IfftMat(:,which_bin);
+end
