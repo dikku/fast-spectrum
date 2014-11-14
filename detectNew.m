d838ec36a47d6ce3f46ccee2010d51ee796656bf
Dinesh Ramasamy <dineshr@ece.ucsb.edu>
2014-11-13 16:40:23 -0800

    fixed typos

--- detectNew.m
+++ detectNew.m
@@ -7,16 +7,23 @@ if sampledManifold.is_eye
     R = length(sampledManifold.coarseOmega);
     N = length(y_r);
     
+    % energy = 1; % for all frequencies
     possible_gains  = fft(y_r, R)/sqrt(N); 
+    
     % phases are wrong at this point 
     % we only correct the phases 
     % for the detected frequency
 else
-    possible_gains = (sampledManifold.map_IfftMat'*y_r)...
-        ./sampledManifold.map_IfftMat_norm_sq.';
+    energy = sampledManifold.map_IfftMat_norm_sq.';
+    possible_gains = (sampledManifold.map_IfftMat'*y_r)./energy;
 end
 
-[~, which_bin]  = max(abs(possible_gains));
+% choose the frequency which reduces the residue by the largest amount
+if sampledManifold.is_eye
+    [~, which_bin]  = max((abs(possible_gains).^2));
+else
+    [~, which_bin]  = max((abs(possible_gains).^2) .* energy);
+end
 
 omega = sampledManifold.coarseOmega(which_bin);
 gain  = possible_gains(which_bin);
